286214a6b2f16dc3e1511f89f552914e1d7a98ab
Nickouwen <71842732+Nickouwen@users.noreply.github.com>
2025-02-15 15:16:05 -0800

    Added movement

--- Studio Assignment 2/Assets/Scripts/BallController.cs
+++ Studio Assignment 2/Assets/Scripts/BallController.cs
@@ -4,22 +4,30 @@ using UnityEngine.Events;
 public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
-    [SerializeField] private InputManager inputManager;
+    [SerializeField] private Transform ballAnchor;
 
     private Rigidbody ballRB;
     private bool isLaunched;
+    private InputManager inputManager;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         ballRB = GetComponent<Rigidbody>();
 
         inputManager.OnSpacePressed.AddListener(LaunchBall);
+
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
     }
 
     private void LaunchBall()
     {
         if (isLaunched) return;
         isLaunched = true;
+        transform.parent = null;
+        ballRB.isKinematic = false;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
